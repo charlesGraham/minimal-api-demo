24965513526c6fbd1255260ee873d66d4fc01c5b
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-18 17:40:16 -0500

    add model validation

--- WebApiDemo/WebApiDemo/Models/Shirt.cs
+++ WebApiDemo/WebApiDemo/Models/Shirt.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiDemo.Models
 {
     public class Shirt
     {
         public int Id { get; set; }
-        public string? Color { get; set; }
-        public string Size { get; set; }
-        public string? Brand { get; set; }
-        public string? Gender { get; set; }
-        public double Price { get; set; }
+
+        [Required]
+        public string Color { get; set; } = string.Empty;
+
+        [Required]
+        public string Size { get; set; } = string.Empty;
+
+        [Required]
+        public string Brand { get; set; } = string.Empty;
+
+        [Required]
+        public string Gender { get; set; } = string.Empty;
+
+        public double? Price { get; set; }
     }
 }
