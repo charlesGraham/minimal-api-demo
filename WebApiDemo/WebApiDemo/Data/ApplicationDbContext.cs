20b8f8624cab754ee2b1ae2b6d4812e6406cfddb
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-19 08:24:19 -0500

    add EF Core + db seeding

---
+++ WebApiDemo/WebApiDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Shirt> Shirts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<Shirt>()
+                .HasData(
+                    new Shirt
+                    {
+                        Id = 1,
+                        Color = "Red",
+                        Size = 10,
+                        Brand = "Nike",
+                        Gender = "Male",
+                        Price = 100,
+                    },
+                    new Shirt
+                    {
+                        Id = 2,
+                        Color = "Blue",
+                        Size = 12,
+                        Brand = "Adidas",
+                        Gender = "Female",
+                        Price = 200,
+                    },
+                    new Shirt
+                    {
+                        Id = 3,
+                        Color = "Green",
+                        Size = 14,
+                        Brand = "Puma",
+                        Gender = "Male",
+                        Price = 300,
+                    }
+                );
+        }
+    }
+}
