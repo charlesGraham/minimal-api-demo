27a95fdc1dff6a29fbb344a116862c73031cad0e
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-06-19 16:34:09 -0500

    add Application class for auth

---
+++ WebApiDemo/WebApiDemo/Authority/AppRepo.cs
@@ -0,0 +1,26 @@
+namespace WebApiDemo.Authority
+{
+    public static class AppRepo
+    {
+        private static List<Application> _applications = new()
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "36854d13-991e-48ea-b481-8bb8c6a58391",
+                ClientSecret = "d071cb9d-4a98-40b2-b903-72f13821aef7",
+            },
+        };
+
+        public static bool Authenticate(string clientId, string secret)
+        {
+            return _applications.Any(app => app.ClientId == clientId && app.ClientSecret == secret);
+        }
+
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(app => app.ClientId == clientId);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Authority/Application.cs
@@ -0,0 +1,10 @@
+namespace WebApiDemo.Authority
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+        public string? AppliationName { get; set; }
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
